99eb0e64dd4f4350f0f8a2d1b7d9d22c08a9fe98
candraprasetiaadiputra <candraprasetya0262gmail.com>
2024-07-30 19:00:08 +0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
